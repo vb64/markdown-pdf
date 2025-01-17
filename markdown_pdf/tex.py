ec3fdd13e388fd3248b73bf2a5d8e1fb62d06f1c
vb64 <vit.sar68@gmail.com>
2025-01-17 21:22:07 +0400

    tex hrefs

---
+++ markdown_pdf/tex.py
@@ -0,0 +1,7 @@
+"""Tex in python.
+
+https://stackoverflow.com/questions/37754756/can-i-integrate-mathjax-into-a-python-program
+https://stackoverflow.com/questions/27882261/mkdocs-and-mathjax
+https://github.com/mayoff/python-markdown-mathjax
+https://pypi.org/project/python-markdown-math/
+"""
