3937ed1f2274bdc9d74b9c669d9ec1957c71fec1
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-03-18 09:14:16 +0400

    fix tests

--- tests/test/__init__.py
+++ tests/test/__init__.py
@@ -12,6 +12,6 @@ class TestBase(TestCase):
         return os.path.join('fixture', *file_name)
 
     @staticmethod
-    def build(file_name):
+    def build(*file_name):
         """Return path to file in 'build' folder."""
-        return os.path.join('build', file_name)
+        return os.path.join('build', *file_name)
