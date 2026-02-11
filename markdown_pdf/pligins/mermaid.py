93e6268d3758eceba20dc6c64f9758cac5f31a4d
vb64 <vit.sar68@gmail.com>
2026-02-11 19:36:08 +0400

    step

--- markdown_pdf/pligins/mermaid.py
+++ markdown_pdf/pligins/mermaid.py
@@ -2,18 +2,17 @@
 import base64
 import requests
 
+from .helpers import get_key
+
 
 def handler(params_dict, text, temp_files):
     """Translate mermaid marked text to png image."""
-    url = 'https://mermaid.ink/img/'
-    if params_dict and ("url" in params_dict):
-        url = params_dict.get("url")
-
-    text = '\n'.join(text.splitlines()[1:-1])
+    url = get_key(params_dict, 'url', 'https://mermaid.ink/img/')
+    code = '\n'.join(text.splitlines()[1:-1])
     response = requests.get(
       "{}{}".format(
         url,
-        base64.urlsafe_b64encode(text.encode("utf-8")).decode("ascii"),
+        base64.urlsafe_b64encode(code.encode("utf-8")).decode("ascii"),
       ),
       params={"format": "png"},
       timeout=5
