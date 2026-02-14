07262da78f2513aca679ad36775189660bb16449
vb64 <vit.sar68@gmail.com>
2026-02-14 10:00:52 +0400

    step

--- tests/test/test_hooks.py
+++ tests/test/test_hooks.py
@@ -4,6 +4,7 @@ https://pymupdf.readthedocs.io/en/latest/tutorial.html
 
 make test T=test_hooks.py
 """
+import fitz
 from . import TestBase
 
 
@@ -12,3 +13,26 @@ class TestHooks(TestBase):
 
     def test_backcolor(self):
         """Check page background color feature."""
+        from markdown_pdf import Section, MarkdownPdf
+
+        pdf = MarkdownPdf()
+        text = open(self.fixture("plantuml.md"), "rt", encoding='utf-8').read()
+        section = Section(text)
+
+        where = section.rect + section.borders
+        html = pdf.m_d.render(section.text)
+        story = fitz.Story(html=html, archive=section.root)
+
+        page = pdf.writer.begin_page(section.rect)
+
+        # https://pymupdf.readthedocs.io/en/latest/the-basics.html#adding-a-watermark-to-a-pdf
+        # insert an image watermark from a file name to fit the page bounds
+        # page.insert_image(page.bound(), filename="watermark.png", overlay=False)
+        # https://pypi.org/project/pypng/
+
+        story.place(where)
+        # story.element_positions(self._recorder, {"toc": section.toc, "pdfile": self})
+        story.draw(page)
+        pdf.writer.end_page()
+
+        pdf.save(self.build('backcolor.pdf'))
