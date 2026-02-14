ac7ecf20e7f219c635be6b02094cf6d185d8aa05
vb64 <vit.sar68@gmail.com>
2026-02-14 12:48:55 +0400

    step

--- tests/test/test_hooks.py
+++ tests/test/test_hooks.py
@@ -23,16 +23,22 @@ class TestHooks(TestBase):
         html = pdf.m_d.render(section.text)
         story = fitz.Story(html=html, archive=section.root)
 
-        page = pdf.writer.begin_page(section.rect)
-
-        # https://pymupdf.readthedocs.io/en/latest/the-basics.html#adding-a-watermark-to-a-pdf
-        # insert an image watermark from a file name to fit the page bounds
-        # page.insert_image(page.bound(), filename="watermark.png", overlay=False)
-        # https://pypi.org/project/pypng/
-
         story.place(where)
         # story.element_positions(self._recorder, {"toc": section.toc, "pdfile": self})
+        page = pdf.writer.begin_page(section.rect)
         story.draw(page)
         pdf.writer.end_page()
 
-        pdf.save(self.build('backcolor.pdf'))
+        fname = self.build('backcolor.pdf')
+        pdf.save(fname)
+
+        print("###")
+        doc = fitz.open(fname)
+        print("#", doc.page_count)
+        # https://pymupdf.readthedocs.io/en/latest/recipes-drawing-and-graphics.html
+        # https://pymupdf.readthedocs.io/en/latest/the-basics.html#adding-a-watermark-to-a-pdf
+        # insert an image watermark from a file name to fit the page bounds
+        page = doc[0]
+        page.insert_image(page.bound(), filename=self.fixture("plantuml.png"), overlay=False)
+        # https://pypi.org/project/pypng/
+        doc.save(self.build('backcolor1.pdf'))
